c206f3b62ea16316733df1a08df8beabeef18459
busra <busra@BR4_YAKUT>
2026-08-08 03:07:09 +0300

    Proje dosyası ekle.

---
+++ BİTANEMM/Pages/Hikaye1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BİTANEMM.Pages
+{
+    public class Hikaye1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
